9512673eb04d0bc9717de704bb32ee62734a660e
Renda Santana <rendasantana@gmail.com>
2024-09-05 17:21:36 +0700

    commit again

--- Object Repository/Get Resource/List resource.rs
+++ Object Repository/Get Resource/List resource.rs
@@ -39,6 +39,14 @@ import internal.GlobalVariable as GlobalVariable
 
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
-ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+
+assertThat(response.getStatusCode()).isIn(Arrays.asList(200, 201, 202))</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
--- Object Repository/Post User/Login Unsuccess - 1.rs
+++ Object Repository/Post User/Login Unsuccess - 1.rs
@@ -51,6 +51,14 @@ import internal.GlobalVariable as GlobalVariable
 
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
-ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 400)
+
+assertThat(response.getStatusCode()).isEqualTo(400)
+
+
+assertThat(response.getStatusCode()).isIn(Arrays.asList(400))</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
